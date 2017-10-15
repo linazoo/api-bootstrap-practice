049e21e4c9f245fe116c9f1addb3aec147eb17d7
Lina Zoo <linazooper@gmail.com>
2017-10-15 14:52:17 -0500

    :started created models validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
   # Remember to create a migration!
+  has_many :subscriptions 
+  has_many :channels, through: :subscriptions
+  # Every user has an email
+  # Every user's email is unique
+  validates :email, presence: true 
+  validates_uniqueness_of :email 
+  # Every user has a password
+  validates :password, presence: true 
+
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  
 end
